bd41aecba16de0809af18acdc409479c177be2dc
instanteast <dyoon0312x@gmail.com>
2025-07-26 06:55:59 +0900

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const fullNotice = `앞쪽부터 빈칸 없이 자리 채워서 앉아주세요.
 
 1. <b>교재/ 컴퓨터 싸인펜/ 화이트</b>가 없는 학생은 <b>조교를 찾아주세요.</b>
 
-2. <b>OMR 수험번호는 010 빼고 학생 전화번호</b> 적어주세요.
+2. <b>OMR 수험번호는 010 제외하고 학생 전화번호</b> 적어주세요.
 
 3. <b>신규 학생</b>은 단어 시험 OMR 윗부분에 <b>신규</b>라고 적고, <b>이름, 학교</b>만 기입 후 시험지에 <b>아는 단어만 체크</b>해 주세요. <b>신규는 재시험 없으니 편하게</b> 보세요.`;
 const breakMsg = '복도에서 각자 자기 주간오렌지 가져가세요';
@@ -34,7 +34,7 @@ function startTimer(seconds, title) {
   } else if (title === '쉬는 시간') {
     subText = breakMsg;
   } else {
-    subText = 'OMR 수험번호는 010 빼고 학생 전화번호 적어주세요.';
+    subText = 'OMR 수험번호는 010 제외하고 학생 전화번호 적어주세요.';
   }
   document.getElementById('timer-subtext').innerHTML = subText;
 
